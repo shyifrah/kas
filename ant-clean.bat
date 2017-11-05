00b73f1b6ac56d2031c24a1aa25e68d7624c3743
Ifrah <shy.ifrah@sap.com>
2017-11-05 10:53:33 +0200

    cleaning all projects

--- ant-clean.bat
+++ ant-clean.bat
@@ -5,22 +5,15 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: extract KAS_HOME
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-set "BATCH_DIR=%~dp0"
-pushd %BATCH_DIR%..
-set "SCRIPT_HOME=%CD%"
-popd
-pushd %SCRIPT_HOME%
-
+set "SCRIPT_HOME=%~dp0"
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: determine which JAR should be excluded from CLASS_PATH and construct it
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-for %%I in (%SCRIPT_HOME%\kas-) do (
-  set "CLEAN_DIR_NAME=%%I"
-  pushd !CLEAN_DIR_NAME!
+for /D %%G in ("%SCRIPT_HOME%/kas-*") do (
+  pushd %%G
   ant Clean
   popd
 )
 
-popd
 exit /b
