dfa111bc0e04aa19e63d1e836a54e389f5094f59
Shy Ifrah <ifrah.shy@gmail.com>
2018-09-17 12:33:30 +0300

    notifying remote servers upon queue deletion/definition

--- kas-mq-server/src/com/kas/mq/server/processors/DefineQueueProcessor.java
+++ kas-mq-server/src/com/kas/mq/server/processors/DefineQueueProcessor.java
@@ -1,9 +1,12 @@
 package com.kas.mq.server.processors;
 
+import java.util.Map;
+import com.kas.comm.impl.NetworkAddress;
 import com.kas.infra.utils.StringUtils;
 import com.kas.mq.impl.IMqMessage;
 import com.kas.mq.impl.internal.EMqCode;
 import com.kas.mq.impl.internal.IMqConstants;
+import com.kas.mq.impl.internal.MqClientImpl;
 import com.kas.mq.impl.internal.MqLocalQueue;
 import com.kas.mq.server.IController;
 
@@ -70,5 +73,50 @@ public class DefineQueueProcessor extends AProcessor
     
     mLogger.debug("DefineQueueProcessor::process() - OUT");
     return respond();
+  }
+  
+  /**
+   * Post-process queue definition request.<br>
+   * <br>
+   * If queue definition was successful, we need to inform remote KAS/MQ managers
+   * that the local repository was updated and that they should update their repository
+   * to reflect these changes.
+   * 
+   * @param reply The reply message the processor's {@link #process()} method generated
+   * @return {@code true} in case the handler should continue process next request,
+   * {@code false} if it should terminate 
+   * 
+   * @see com.kas.mq.server.processors.IProcessor#postprocess(IMqMessage, IMqMessage)
+   */
+  public boolean postprocess(IMqMessage<?> reply)
+  {
+    mLogger.debug("DefineQueueProcessor::postprocess() - IN");
+    
+    if (mCode == EMqCode.cOkay)
+    {
+      Map<String, NetworkAddress> map = mConfig.getRemoteManagers();
+      String localQmgr = mConfig.getManagerName();
+      
+      for (Map.Entry<String, NetworkAddress> entry : map.entrySet())
+      {
+        String remoteQmgrName  = entry.getKey();
+        NetworkAddress address = entry.getValue();
+        
+        mLogger.debug("DefineQueueProcessor::postprocess() - Notifying KAS/MQ server \"" + remoteQmgrName + "\" (" + address.toString() + ") on repository update");
+        
+        MqClientImpl client = new MqClientImpl();
+        client.connect(address.getHost(), address.getPort());
+        if (client.isConnected())
+        {
+          boolean success = client.notifyRepoUpdate(localQmgr, mQueue, true);
+          mLogger.debug("DefineQueueProcessor::postprocess() - Notification returned: " + success);
+          
+          client.disconnect();
+        }
+      }
+    }
+    
+    mLogger.debug("DefineQueueProcessor::postprocess() - OUT");
+    return true;
   }
 }
