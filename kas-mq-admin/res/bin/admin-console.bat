91c4bf7fdb587cd7fa63f3b686bc00bbcca0deec
Shy Ifrah <ifrah.shy@gmail.com>
2018-09-17 23:25:13 +0300

    update admin console batch script following last refactoring

--- kas-mq-admin/res/bin/admin-console.bat
+++ kas-mq-admin/res/bin/admin-console.bat
@@ -3,4 +3,4 @@
 @echo off
 
 set "BATCH_DIR=%~dp0"
-call %BATCH_DIR%/launcher.bat kas.home=. kas.class=com.kas.mq.admcons.KasMqAdmin
+call %BATCH_DIR%/launcher.bat kas.home=. kas.class=com.kas.mq.admin.KasMqAdmin
