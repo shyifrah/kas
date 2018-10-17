2f421b49c05b4773785e0cb7abdde62c0c004f74
Shy Ifrah <ifrah.shy@gmail.com>
2018-10-17 21:41:23 +0300

    javadoc

--- kas-sec-core/src/com/kas/sec/AccessEntry.java
+++ kas-sec-core/src/com/kas/sec/AccessEntry.java
@@ -3,31 +3,78 @@ package com.kas.sec;
 import java.util.regex.Pattern;
 import com.kas.infra.base.AKasObject;
 
+/**
+ * An access entry is a an entry in an access list that protects a resource(s).<br>
+ * <br>
+ * The resources protected by this entry are designated by the regular expression {@code mResourceRegEx}
+ * and the permitted access level by the {@code mPermittedAccess}.
+ * 
+ * @author Pippo
+ */
 public class AccessEntry extends AKasObject
 {
+  /**
+   * Resource regular expression
+   */
   private Pattern mResourceRegEx;
+  
+  /**
+   * Permitted access level
+   */
   private AccessLevel mPermittedAccess;
   
+  /**
+   * Construct an access-entry
+   * 
+   * @param resource A regular expression that identifies the resources protected by this entry
+   * @param accessLevel The permitted level of access
+   */
   AccessEntry(String resource, AccessLevel accessLevel)
   {
     mResourceRegEx = Pattern.compile(resource);
     mPermittedAccess = accessLevel;
   }
   
+  /**
+   * Test if {@code resource} is protected by this {@link AccessEntry}.
+   * 
+   * @param resource The resource tested
+   * @return {@code true} if this {@link AccessEntry} protects {@code resource}.
+   */
   public boolean isMatched(String resource)
   {
     return mResourceRegEx.matcher(resource).matches();
   }
   
+  /**
+   * Get the access level allowed for the specified resource
+   * 
+   * @param resource The resource tested
+   * @return the allowed {@link AccessLevel}
+   */
   public AccessLevel getAllowedAccessLevel(String resource)
   {
-    if (mResourceRegEx.matcher(resource).matches())
+    if (isMatched(resource))
       return mPermittedAccess;
     return AccessLevel.NONE_ACCESS;
   }
   
+  /**
+   * Get the object's detailed string representation
+   * 
+   * @param level The string padding level
+   * @return the string representation with the specified level of padding
+   * 
+   * @see com.kas.infra.base.IObject#toPrintableString(int)
+   */
   public String toPrintableString(int level)
   {
-    return null;
+    String pad = pad(level);
+    StringBuilder sb = new StringBuilder();
+    sb.append(name()).append("(\n")
+      .append(pad).append("  RegEx=").append(mResourceRegEx.toString()).append("\n")
+      .append(pad).append("  Access=").append(mPermittedAccess.toPrintableString(level+1)).append("\n")
+      .append(pad).append(")");
+    return sb.toString();
   }
 }
--- kas-sec-core/src/com/kas/sec/AccessLevel.java
+++ kas-sec-core/src/com/kas/sec/AccessLevel.java
@@ -3,8 +3,25 @@ package com.kas.sec;
 import com.kas.infra.base.AKasObject;
 import com.kas.infra.utils.StringUtils;
 
+/**
+ * {@link AccessLevel} describes a level of access that is granted to a user
+ * upon a specific resource.<br>
+ * <br>
+ * The access level is actually a bit of a byte. For example:<br>
+ *   READ access:     00000001<br>
+ *   WRITE access:    00000010<br>
+ * If we wish to grant a user a READ and WRITE access we simply turn on both bits:<br>
+ *   READ-WRITE:      00000011<br>
+ * Of course, not all resource classes has a meaning for write access vs. read access.
+ * In these resource classes, we would probably like to enable only the read access.
+ * 
+ * @author Pippo
+ */
 public class AccessLevel extends AKasObject
 {
+  /**
+   * Access bits
+   */
   static public final byte READ  = 1;   // 00000001
   static public final byte WRITE = 2;   // 00000010
   static public final byte ALTER = 4;   // 00000100
@@ -14,28 +31,58 @@ public class AccessLevel extends AKasObject
   static public final byte PH_04 = 64;  // 01000000
   static public final byte PH_05 = -1;  // 10000000
   
+  /**
+   * Access levels
+   */
   static public final AccessLevel NONE_ACCESS  = new AccessLevel((byte)0);
   static public final AccessLevel READ_ACCESS  = new AccessLevel(READ);
   static public final AccessLevel WRITE_ACCESS = new AccessLevel(WRITE);
   static public final AccessLevel ALTER_ACCESS = new AccessLevel(ALTER);
   
+  /**
+   * The access level
+   */
   private byte mAccessLevel;
   
+  /**
+   * Construct an access level with the specified byte.
+   * 
+   * @param accessLevel The bit-string that should be turned on 
+   */
   AccessLevel(byte accessLevel)
   {
     mAccessLevel = accessLevel;
   }
   
-  public boolean isLevelEnabled(byte level)
+  /**
+   * Get an indication if access levels are enabled or not 
+   *  
+   * @param levels A bit-string of access levels
+   * @return {@code true} if all bits of {@code levels} are turned on, {@code false} otherwise
+   */
+  public boolean isLevelEnabled(byte levels)
   {
-    return (mAccessLevel & level) != 0;
+    return (mAccessLevel & levels) != 0;
   }
   
+  /**
+   * Get the bit-string as a hexa-decimal character.
+   * 
+   * @return the bit-string as a hexa-decimal character.
+   */
   public String toString()
   {
     return StringUtils.asHexString(new byte [] {mAccessLevel});
   }
   
+  /**
+   * Get the object's detailed string representation
+   * 
+   * @param level The string padding level
+   * @return the string representation with the specified level of padding
+   * 
+   * @see com.kas.infra.base.IObject#toPrintableString(int)
+   */
   public String toPrintableString(int level)
   {
     StringBuilder sb = new StringBuilder().append("AccessLevels=");
