4c0732b9201bb332a207bb4f52f765f42536c237
Shy Ifrah <ifrah.shy@gmail.com>
2018-10-26 22:20:13 +0300

    added some initial INSERT instructions

--- kas-data/res/conf/db-init-mysql.sql
+++ kas-data/res/conf/db-init-mysql.sql
@@ -5,18 +5,26 @@ CREATE TABLE ${kas.mq.db.schema}.kas_mq_users (
   description VARCHAR(100),
   password    VARCHAR(50)
 );
-      
+
+INSERT INTO ${kas.mq.db.schema}.kas_mq_users (name, description, password)
+  VALUES('root', 'system root', 'root');
+
 DROP TABLE IF EXISTS ${kas.mq.db.schema}.kas_mq_groups;
 CREATE TABLE ${kas.mq.db.schema}.kas_mq_groups (
   id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name        VARCHAR(20) NOT NULL UNIQUE,
   description VARCHAR(100)
 );
-      
+
+INSERT INTO ${kas.mq.db.schema}.kas_mq_groups (name, description)
+  VALUES('system', 'system');
+
 DROP TABLE IF EXISTS ${kas.mq.db.schema}.kas_mq_users_to_groups;
 CREATE TABLE ${kas.mq.db.schema}.kas_mq_users_to_groups (
   user_id     INT NOT NULL,
   group_id    INT NOT NULL,
   PRIMARY KEY(user_id, group_id)
 );
- 
+
+INSERT INTO ${kas.mq.db.schema}.kas_mq_users_to_groups (user_id, group_id)
+  VALUES (1, 1);
