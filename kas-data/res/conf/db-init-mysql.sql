0385adae825d1910547ae4c7fb135b9104a5d20a
Shy Ifrah <ifrah.shy@gmail.com>
2018-10-31 21:27:31 +0200

    temp: update init script for mysql

--- kas-data/res/conf/db-init-mysql.sql
+++ kas-data/res/conf/db-init-mysql.sql
@@ -6,24 +6,24 @@ DROP TABLE IF EXISTS kas_mq_groups CASCADE;
 DROP TABLE IF EXISTS kas_mq_resource_classes; 
 
 CREATE TABLE kas_mq_users (
-  id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id          INT NOT NULL PRIMARY KEY,
   name        VARCHAR(20) NOT NULL UNIQUE,
   description VARCHAR(100),
   password    VARCHAR(50)
 );
 
-INSERT INTO kas_mq_users (name, description, password)
-  VALUES('root', 'system root', 'root');
-
+-- INSERT INTO kas_mq_users (name, description, password)
+--   VALUES('root', 'system root', 'root');
+-- 
 CREATE TABLE kas_mq_groups (
-  id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id          INT NOT NULL PRIMARY KEY,
   name        VARCHAR(20) NOT NULL UNIQUE,
   description VARCHAR(100)
 );
 
-INSERT INTO kas_mq_groups (name, description)
-  VALUES('system', 'system');
-
+-- INSERT INTO kas_mq_groups (name, description)
+--   VALUES('system', 'system');
+-- 
 CREATE TABLE kas_mq_users_to_groups (
   user_id     INT NOT NULL,
   group_id    INT NOT NULL,
@@ -32,26 +32,27 @@ CREATE TABLE kas_mq_users_to_groups (
   FOREIGN KEY fk_group_id(group_id) REFERENCES kas_mq_groups(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO kas_mq_users_to_groups
-  SELECT user_id, group_id FROM 
-  (
-    SELECT id user_id
-    FROM kas_mq_users
-    WHERE name ='root'
-  ) users,
-  (
-    SELECT id group_id
-    FROM kas_mq_groups 
-    WHERE name = 'system'
-  ) groups;
-
+-- INSERT INTO kas_mq_users_to_groups
+--   SELECT user_id, group_id FROM 
+--   (
+--     SELECT id user_id
+--     FROM kas_mq_users
+--     WHERE name ='root'
+--   ) users,
+--   (
+--     SELECT id group_id
+--     FROM kas_mq_groups 
+--     WHERE name = 'system'
+--   ) groups;
+-- 
 CREATE TABLE kas_mq_resource_classes (
-  id            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id            INT NOT NULL PRIMARY KEY,
   name          VARCHAR(32) NOT NULL UNIQUE,
   access_levels INT
 );
 
-INSERT INTO kas_mq_resource_classes
-  VALUES('application', 1);
-INSERT INTO kas_mq_resource_classes
-  VALUES('queue', 7);
+-- INSERT INTO kas_mq_resource_classes
+--   VALUES('application', 1);
+-- INSERT INTO kas_mq_resource_classes
+--   VALUES('queue', 7);
+-- 
