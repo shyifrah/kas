1d4915171d0462479665e4be20cbce5d8766c3eb
Shy Ifrah <ifrah.shy@gmail.com>
2018-10-31 21:32:12 +0200

    add methods to check if db schema was initialized and initialize it

--- kas-db/src/com/kas/db/DbUtils.java
+++ kas-db/src/com/kas/db/DbUtils.java
@@ -1,5 +1,20 @@
 package com.kas.db;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import com.kas.config.MainConfiguration;
+import com.kas.infra.base.Properties;
+import com.kas.infra.base.PropertyResolver;
+import com.kas.infra.utils.FileUtils;
+import com.kas.infra.utils.RunTimeUtils;
+import com.kas.infra.utils.StringUtils;
+import com.kas.logging.ILogger;
+import com.kas.logging.LoggerFactory;
+
 /**
  * A set of DB-related utility functions
  * 
@@ -7,6 +22,8 @@ package com.kas.db;
  */
 public class DbUtils
 {
+  static private ILogger sLogger = LoggerFactory.getLogger(DbUtils.class);
+  
   /**
    * Create a connection URL
    * 
@@ -34,5 +51,130 @@ public class DbUtils
     }
     
     return sb.toString();
+  }
+  
+  /**
+   * Check if the schema was initialized
+   * 
+   * @return {@code true} if the schema was initialized, {@code false} otherwise
+   */
+  static public boolean isSchemaInitialized()
+  {
+    sLogger.debug("DbUtils::isSchemaInitialized() - IN");
+    boolean init = false;
+    
+    DbConnectionPool pool = DbConnectionPool.getInstance();
+    DbConnection dbConn = pool.allocate();
+    Connection conn = dbConn.getConn();
+    
+    String sql = "select column_name, data_type from information_schema.columns where table_name = 'kas_mq_users';";
+    try
+    {
+      PreparedStatement ps = conn.prepareStatement(sql);
+      ResultSet rs =  ps.executeQuery();
+      if (rs.next()) init = true;
+    }
+    catch (SQLException e)
+    {
+      init = false;
+    }
+    
+    pool.release(dbConn);
+    sLogger.debug("DbUtils::isSchemaInitialized() - OUT, Returns=" + init);
+    return init;
+  }
+  
+  /**
+   * Initialize KAS/MQ database schema
+   */
+  static public void initSchema()
+  {
+    sLogger.debug("DbUtils::initSchema() - IN");
+    
+    Properties props = MainConfiguration.getInstance().getSubset(DbConfiguration.cDbConfigPrefix);
+    String dbtype = props.getStringProperty(DbConfiguration.cDbConfigPrefix + "dbtype", DbConfiguration.cDefaultDbType);
+    
+    sLogger.debug("DbUtils::initSchema() - Database type is " + dbtype);
+    
+    File dbInitFile = new File(RunTimeUtils.getProductHomeDir() + File.separator + "conf" + File.separator + "db-init-" + dbtype + ".sql");
+    List<String> input = FileUtils.load(dbInitFile, "--");
+    
+    boolean newcmd = true;
+    StringBuilder sb = null;
+    for (String line : input)
+    {
+      line = line.trim();
+      sLogger.diag("DbUtils::initSchema() - Current line: [" + line + "]");
+      
+      if (newcmd)
+      {
+        sLogger.diag("DbUtils::initSchema() - Current line is the start of a new command");
+        sb = new StringBuilder(line);
+        newcmd = false;
+      }
+      else
+      {
+        sLogger.diag("DbUtils::initSchema() - Current line is a continuation of the previous command, concatenating...");
+        sb.append(' ').append(line);
+      }
+      
+      if (line.endsWith(";"))
+      {
+        sLogger.diag("DbUtils::initSchema() - End of command detected. Execute it...");
+        String cmd = PropertyResolver.resolve(sb.toString(), props);
+        try
+        {
+          execute(cmd);
+        }
+        catch (SQLException e) {}
+        newcmd = true;
+      }
+    }
+    sLogger.debug("DbUtils::initSchema() - OUT");
+  }
+  
+  /**
+   * Execute a SQL statement
+   * 
+   * @param sql The SQL statement
+   * @return A result set, if one was generated
+   * @throws SQLException if thrown by java.sql.* classes
+   */
+  static public ResultSet execute(String sql) throws SQLException
+  {
+    return execute(sql, new Object [] {});
+  }
+  
+  /**
+   * Execute a SQL statement
+   * 
+   * @param fmt The SQL statement base format
+   * @param args Arguments to format the SQL
+   * @return A result set, if one was generated
+   * @throws SQLException if thrown by java.sql.* classes
+   */
+  static public ResultSet execute(String fmt, Object... args) throws SQLException
+  {
+    sLogger.debug("DbUtils::execute() - IN");
+    
+    DbConnectionPool pool = DbConnectionPool.getInstance();
+    DbConnection dbConn = pool.allocate();
+    Connection conn = dbConn.getConn();
+    
+    String sql = String.format(fmt, args);
+    ResultSet result = null;
+
+    sql = StringUtils.clearBlanks(sql);
+    sLogger.trace("DbUtils::execute() - Executing SQL: [" + sql + "]");
+    PreparedStatement st = conn.prepareStatement(sql);
+    boolean b = st.execute();
+    if (b) result = st.getResultSet();
+    
+    sLogger.trace("DbUtils::execute() - HaveResult: [" + b + "]");
+    
+    pool.release(dbConn);
+    
+    sLogger.debug("DbUtils::execute() - OUT");
+    return result;
   }
 }
