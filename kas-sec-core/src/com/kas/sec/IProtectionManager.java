e00c1b01be04a37f183651359b67f79bde48f773
Shy Ifrah <ifrah.shy@gmail.com>
2018-11-04 12:29:02 +0200

    continue diagram

--- kas-sec-core/src/com/kas/sec/IProtectionManager.java
+++ kas-sec-core/src/com/kas/sec/IProtectionManager.java
@@ -7,29 +7,45 @@ import com.kas.sec.entities.UserEntity;
  * It is responsible for managing all entities, resource classes and permissions.
  * A schematic figure of the relationships between object in this project:
  * <code><br>
- *  +----------------------------+                             +------------+------------------+<br>
- *  |                            |    +-------------------->>> | <userid_1> | <user-entity_1>  |<br>
- *  |    IProtectionManager      |    |                        | <userid_2> | <user-entity_2>  |<br>
- *  |                            |    |                        | <userid_3> | <user-entity_3>  |<br>
- *  +----------------------------+    |                        | :          | :                |<br>
- *  | mUsers               >>>---|----+                        | <userid_n> | <user-entity_n>  |<br>
- *  | mGroups              >>>---|--------------------+        +------------+------------------+<br>
- *  | mResourcesAccessList >>>---|----+               |                                         <br>
- *  +----------------------------+    |               |        +------------+------------------+<br>
- *                                    |               +---->>> | <grpid_1>  | <group-entity_1> |<br>
- *                                    |                        | <grpid_2>  | <group-entity_2> |<br>
- *                                    |                        | <grpid_3>  | <group-entity_3> |<br>
- *                                    |                        | :          | :                |<br>
- *                                    |                        | <grpid_n>  | <group-entity_n> |<br>
- *                                    |                        +------------+------------------+<br>
- *                                    |                                                          <br>
- *                                    |                        +--------------+-----------------+<br>
- *                                    +-------------------->>> | <resclass_1> | <access-list_1> |<br>
- *                                                             | <resclass_2> | <access-list_2> |<br>
- *                                                             | <resclass_3> | <access-list_3> |<br>
- *                                                             | :            | :               |<br>
- *                                                             | <resclass_n> | <access-list_n> |<br>
- *                                                             +--------------+-----------------+<br>
+ *  +----------------------------+                       +------------+------------------+<br>
+ *  |                            |    +-------------->>> | <userid_1> | <user-entity_1>  |<br>
+ *  |    IProtectionManager      |    |                  | <userid_2> | <user-entity_2>  |<br>
+ *  |                            |    |                  | <userid_3> | <user-entity_3>  |<br>
+ *  +----------------------------+    |                  | :          | :                |<br>
+ *  | mUsers               >>>---|----+                  | <userid_n> | <user-entity_n>  |<br>
+ *  | mGroups              >>>---|--------------+        +------------+------------------+<br>
+ *  | mResourcesAccessList >>>---|----+         |                                         <br>
+ *  +----------------------------+    |         |        +------------+------------------+<br>
+ *                                    |         +---->>> | <grpid_1>  | <group-entity_1> |<br>
+ *                                    |                  | <grpid_2>  | <group-entity_2> |<br>
+ *                                    |                  | <grpid_3>  | <group-entity_3> |<br>
+ *                                    |                  | :          | :                |<br>
+ *                                    |                  | <grpid_n>  | <group-entity_n> |<br>
+ *                                    |                  +------------+------------------+<br>
+ *                                    |                                                   <br>
+ *                                    |                 +--------------+-----------------+<br>
+ *                                    +------------->>> | <resclass_1> | <access-list_1> |<br>
+ *                                                      | <resclass_2> | <access-list_2> |<br>
+ *                                                      | <resclass_3> | <access-list_3> |<br>
+ *                                                      | :            | :               |<br>
+ *                                                      | <resclass_n> | <access-list_n> | >>>---+ <br>
+ *                                                      +--------------+-----------------+       | <br>
+ *                                                                                               | <br>
+ *                                                                                               | <br>
+ *                                                                                               | <br>
+ *       +---------------------------------------------------------------------------------------+ <br>
+ *       |                                                                                         <br>
+ *       |                                                                                         <br>
+ *       |            +------------------+                                                         <br>
+ *       +-------->>> | <access-entry_1> |                                                         <br>
+ *                    | <access-entry_2> |                                                         <br>
+ *                    | <access-entry_3> |                                                         <br>
+ *                    | :                |                                                         <br>
+ *                    | <access-entry_n> | >>>---+
+ *                    +------------------+       |
+ *                                               |
+ *                                               |
+ *                                               |
  *                   
  * </code>
  * 
