7ba9a79c47b9da46b8d8004693c835b141d84ce2
Shy Ifrah <ifrah.shy@gmail.com>
2018-11-05 12:10:38 +0200

    update comments and add UNKNOWN resource class

--- kas-sec-core/src/com/kas/sec/resources/EResourceType.java
+++ kas-sec-core/src/com/kas/sec/resources/EResourceType.java
@@ -2,8 +2,20 @@ package com.kas.sec.resources;
 
 import com.kas.sec.access.AccessLevel;
 
+/**
+ * This ENUM is used to simplify the the code when requesting to check
+ * permissions for a user against a specific resource.<br>
+ * 
+ * The values of this ENUM <b>must</b> match the contents of {@code table kas_mq_resource_types}.
+ * @author Pippo
+ */
 public enum EResourceType
 {
+  /**
+   * Unknown resource type
+   */
+  UNKNOWN(AccessLevel.NONE),
+  
   /**
    * Protects server shutdown
    */
