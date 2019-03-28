ef3b7dd0ec55aeaee6df7bdd0fcfa7ef67e2c639
Shy Ifrah <ifrah.shy@gmail.com>
2019-03-28 17:06:26 +0200

    added kas.home const

--- kas-appl/src/main/java/com/kas/appl/IAppConstants.java
+++ kas-appl/src/main/java/com/kas/appl/IAppConstants.java
@@ -5,5 +5,6 @@ public interface IAppConstants
   static public final String cLog4jConfigFileProperty    = "log4j.configurationFile";
   static public final String cLog4jLogDirectoryProperty  = "kas.log.dir";
   static public final String cLog4jLogFileProperty       = "kas.log.file";
+  static public final String cKasHomeDirectory           = "kas.home";
   static public final String cKasExecutableClass         = "kas.class";
 }
